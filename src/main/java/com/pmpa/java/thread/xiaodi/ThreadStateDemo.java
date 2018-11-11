2293d19bc67dce214ebe620803041f561a6b0ac7
maning14 <maning14@meituan.com>
2018-11-11 10:22:16 +0800

    001

---
+++ src/main/java/com/pmpa/java/thread/xiaodi/ThreadStateDemo.java
@@ -0,0 +1,15 @@
+package com.pmpa.java.thread.xiaodi;
+
+
+/**
+ * @Description 通过源码查看Thread的几个状态。
+ *              在jconsole中查看创建的几个状态的实例：
+ *              Runnable, TIME_WAITING
+ * @date 2018-11-11
+ */
+public class ThreadStateDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/pmpa/java/thread/xiaodi/demo/MyRunnable.java
@@ -0,0 +1,4 @@
+package com.pmpa.java.thread.xiaodi.demo;
+
+public class MyRunnable {
+}
