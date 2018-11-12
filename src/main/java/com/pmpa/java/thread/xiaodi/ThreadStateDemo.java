2aea0c453bb47f56b62638a7a289dd61a5b4d52c
maning14 <maning14@meituan.com>
2018-11-12 21:43:24 +0800

    代码实现查看线程的状态

--- src/main/java/com/pmpa/java/thread/xiaodi/ThreadStateDemo.java
+++ src/main/java/com/pmpa/java/thread/xiaodi/ThreadStateDemo.java
@@ -1,19 +1,35 @@
 package com.pmpa.java.thread.xiaodi;
 
 
+import java.io.IOException;
+
 /**
  * @Description 通过源码查看Thread的几个状态。
  *              在jconsole中查看创建的几个状态的实例：
- *              Runnable, TIME_WAITING
+ *                  新建连接 -> 选择这个开发的类 -> 选择"线程"标签 -> 选择我们开发的线程 就可以看到这个线程的状态。
+ *              NEW,RUNNABLE,BLOCKED,WAITING,TIMED_WAITING,TERMINATED
  * @date 2018-11-11
+ * @author natty
+ *
  */
 public class ThreadStateDemo {
 
-
     public static void main(String[] args) {
-       Thread th = new Thread();
-       th.start();
 
-
+        /*RUNNABLE:
+        Thread state for a runnable thread.  A thread in the runnable
+        state is executing in the Java virtual machine but it may
+        be waiting for other resources from the operating system
+        such as processor.
+        */
+        Thread th = new Thread(()->{
+            System.out.println(Thread.currentThread().getName());
+            try {
+                System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        th.start();
     }
 }
