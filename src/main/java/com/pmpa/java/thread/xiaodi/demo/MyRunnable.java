191d792c5f92a06b28e83d89fdfeb1b3593b171c
maning14 <maning14@meituan.com>
2018-11-12 16:45:23 +0800

    实现Runnable接口方式来创建线程

--- src/main/java/com/pmpa/java/thread/xiaodi/demo/MyRunnable.java
+++ src/main/java/com/pmpa/java/thread/xiaodi/demo/MyRunnable.java
@@ -1,4 +1,24 @@
 package com.pmpa.java.thread.xiaodi.demo;
 
-public class MyRunnable {
+/**
+ *
+ * @Description 实现Runnable接口方式来创建线程。
+ * 说明：一般选择这种方式来创建线程。因为Java只允许单继承，
+ *  如果一个类继承了Thread后，就不能再继承其他的类了。
+ *
+ * @date 2018-11-12
+ * @author natty
+ *
+ */
+
+public class MyRunnable implements Runnable {
+    //实现run()方法
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " running");
+    }
+
+    public static void main(String[] args) {
+        Thread th = new Thread(new MyRunnable());
+        th.start();
+    }
 }
