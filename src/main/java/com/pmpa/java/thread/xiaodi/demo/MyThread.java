29fa3f501f887e0c946af5446fd78ea8363f44c5
maning14 <maning14@meituan.com>
2018-11-12 17:56:48 +0800

    继承Thread方式创建线程

--- src/main/java/com/pmpa/java/thread/xiaodi/demo/MyThread.java
+++ src/main/java/com/pmpa/java/thread/xiaodi/demo/MyThread.java
@@ -1,4 +1,22 @@
 package com.pmpa.java.thread.xiaodi.demo;
 
-public class MyThread {
+/**
+ * @Description 继承Thread方式来创建线程
+ *
+ * @Date 2018-11-12
+ * @Author Natty
+ *
+ */
+
+public class MyThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+ " running");
+    }
+
+    public static void main(String[] args) {
+        MyThread th = new MyThread();
+        th.start();
+    }
 }
